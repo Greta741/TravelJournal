8ada409991ba688db47b178e0e4d016d1723a4e2
Greta Žebrauskaitė <greta.zebrauskaite@gmail.com>
2016-12-19 20:38:28 +0200

    no message

--- services/sitemapService.js
+++ services/sitemapService.js
@@ -6,10 +6,10 @@ const generateSitemap = () => {
     let sitemap = '<?xml version="1.0" encoding="UTF-8"?>';
     sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
-    sitemap += '<url><loc>http:tavel5.herokuapp.com/</loc></url>';
+    sitemap += '<url><loc>http:travel5.herokuapp.com/</loc></url>';
     mongoService.getAllJourneysId((result) => {
         result.forEach((item) => {
-            sitemap += `<url><loc>http:tavel5.herokuapp.com/journey/${item._id}</loc></url>`;
+            sitemap += `<url><loc>http:travel5.herokuapp.com/journey/${item._id}</loc></url>`;
         });
         sitemap += '</urlset>';
         fs.writeFile("sitemap.xml", sitemap); 
